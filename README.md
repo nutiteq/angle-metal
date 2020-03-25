7eff3087755ae40329dfcb23e67d9ecd5247c873
mtehver <mark.tehver@gmail.com>
2020-03-25 13:39:57 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,24 @@
+## Prebuilt ANGLE binary for iOS using Metal (arm64, armv7s, x86_64)
+
+Based on ANGLE master branch, current version is [8590cd1c74a576bd99c88f46cf07e33c845f57a5](https://github.com/kakashidinho/metalangle/).
+
+### Compiling instructions
+
+Set these parameters for both MetalANGLE and MetalANGLE_ios targets:
+
+```
+Valid Architectures: arm64 armv7s x86 x86_64
+Build Active Architecture Only: Yes
+Mach-O Type: Static Library
+Symbols Hidden by Default: No
+```
+
+After changing the targets the static libraries can be built for all targets:
+
+```
+cd ios/xcode
+xcodebuild -showsdks
+xcodebuild -project OpenGLES.xcodeproj -configuration Release -arch arm64 -sdk iphoneos13.2 build
+xcodebuild -project OpenGLES.xcodeproj -configuration Release -arch armv7s -sdk iphoneos13.2 build
+xcodebuild -project OpenGLES.xcodeproj -configuration Release -arch x86_64 -sdk iphonesimulator13.2 build
+```
---
+++ include/MGLKView.h
@@ -0,0 +1,50 @@
+//
+// Copyright 2019 Le Hoang Quyen. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#ifndef MGLKView_h
+#define MGLKView_h
+
+#import "MGLContext.h"
+
+@class MGLKView;
+
+@protocol MGLKViewDelegate <NSObject>
+
+// Implement this method to draw to the view using current OpenGL
+// context associated with the view.
+- (void)mglkView:(MGLKView *)view drawInRect:(CGRect)rect;
+
+@end
+
+// NOTE: do not subclass this class, use delegate if needed to override
+// the drawing method.
+@interface MGLKView : MGLKNativeView
+
+@property(nonatomic) MGLContext *context;
+@property(nonatomic, assign) IBOutlet id<MGLKViewDelegate> delegate;
+
+// Default value is NO. Setting to YES will keep the framebuffer data after presenting.
+// Doing so will reduce performance and increase memory usage.
+@property(nonatomic) BOOL retainedBacking;
+@property(nonatomic) MGLDrawableColorFormat drawableColorFormat;      // Default is RGBA8888
+@property(nonatomic) MGLDrawableDepthFormat drawableDepthFormat;      // Default is DepthNone
+@property(nonatomic) MGLDrawableStencilFormat drawableStencilFormat;  // Default is StencilNone
+@property(nonatomic)
+    MGLDrawableMultisample drawableMultisample;  // Default is MGLDrawableMultisampleNone
+
+// Return the size of the OpenGL default framebuffer.
+@property(readonly) CGSize drawableSize;
+
+#if TARGET_OS_IOS
+@property(readonly, strong) UIImage *snapshot;
+#endif
+
+// Redraw the view's contents immediately.
+- (void)display;
+
+@end
+
+#endif /* MLKView_h */
